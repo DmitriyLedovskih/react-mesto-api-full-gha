1c36a1d4d1d17376b3c3d2707956159b28bf7125
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-05-22 20:49:37 +0700

    Fix: исправил баги

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -86,12 +86,11 @@ const login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'some-key',
         { expiresIn: '7d' },
       );
-      console.log(NODE_ENV === 'production' ? JWT_SECRET : 'some-key');
 
       res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        secure: true,
       }).send({ email });
     })
     .catch(next);
