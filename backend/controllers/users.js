1139b73f67a0670a629daa3295e5ed592ecae9d1
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2026-02-15 10:07:40 +0700

    fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,22 +1,23 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { ValidationError } = require('mongoose').Error;
-const User = require('../models/user');
-const {
-  CREATE_STATUS,
-} = require('../utils/constants');
-const BadRequestError = require('../errors/BadRequestError');
-const ConflictError = require('../errors/ConflictError');
-const sendUser = require('../utils/sendUser');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ValidationError } = require("mongoose").Error;
+const User = require("../models/user");
+const { CREATE_STATUS } = require("../utils/constants");
+const BadRequestError = require("../errors/BadRequestError");
+const ConflictError = require("../errors/ConflictError");
+const sendUser = require("../utils/sendUser");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const updateData = (req, res, next) => {
-  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, req.body, {
+    new: true,
+    runValidators: true,
+  })
     .then((user) => sendUser(res, user))
     .catch((err) => {
       if (err instanceof ValidationError) {
-        next(new BadRequestError('Переданы некорректные данные'));
+        next(new BadRequestError("Переданы некорректные данные"));
       } else {
         next(err);
       }
@@ -43,14 +44,9 @@ const getMe = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  } = req.body;
-  bcrypt.hash(password, 10)
+  const { name, about, avatar, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
     .then((hash) => {
       User.create({
         name,
@@ -62,9 +58,9 @@ const createUser = (req, res, next) => {
         .then((user) => res.status(CREATE_STATUS).send({ data: user }))
         .catch((err) => {
           if (err.code === 11000) {
-            next(new ConflictError('Email уже занят'));
+            next(new ConflictError("Email уже занят"));
           } else if (err instanceof ValidationError) {
-            next(new BadRequestError('Переданы некорректные данные'));
+            next(new BadRequestError("Переданы некорректные данные"));
           } else {
             next(err);
           }
@@ -83,21 +79,23 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'some-key',
-        { expiresIn: '7d' },
+        NODE_ENV === "production" ? JWT_SECRET : "some-key",
+        { expiresIn: "7d" },
       );
 
-      res.cookie('token', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      }).send({ email });
+      res
+        .cookie("token", token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: false,
+        })
+        .send({ email });
     })
     .catch(next);
 };
 
 const signOut = (req, res) => {
-  res.clearCookie('token').send({ message: 'Вы вышли из системы' });
+  res.clearCookie("token").send({ message: "Вы вышли из системы" });
 };
 
 module.exports = {
