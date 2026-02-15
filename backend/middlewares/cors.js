23b4685b70ab484e933988632c3df31e5c81425e
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2026-02-15 09:28:49 +0700

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,24 +1,28 @@
 const allowedCors = [
-  'https://mesto.dmitriyledovskih.nomoredomains.monster',
-  'http://mesto.dmitriyledovskih.nomoredomains.monster',
-  'http://localhost:3000',
+  process.env.API_URL,
+  process.env.APP_URL,
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://dmitriyledovskih.github.io",
+  "https://dmitriyledovskih.github.io/react-mesto-api-full-gha",
+  "https://dmitriyledovskih.github.io/react-mesto-api-full-gha/",
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers["access-control-request-headers"];
 
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Credentials", true);
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
   }
 
